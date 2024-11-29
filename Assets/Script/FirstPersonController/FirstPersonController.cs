4133d1973df2e48099d6c2a462a0b0fc814f4878
Lukas <lukas_flores.8@hotmail.com>
2024-11-29 14:53:19 -0300

    fix folders

---
+++ Assets/Script/FirstPersonController/FirstPersonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+
+    // Variables para movimiento de la cámara
+    public Camera playerCamera;
+    public float mouseSensitivity = 2f;
+    public float maxLookAngle = 50f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    private void Update()
+    {
+        HandleCameraMovement();
+    }
+
+    
+    private void HandleCameraMovement()
+    {
+        yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= mouseSensitivity * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
+
+        transform.localEulerAngles = new Vector3(0, yaw, 0);
+        playerCamera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
+    }
+}
